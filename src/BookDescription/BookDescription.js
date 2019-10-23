9c9ea6f4a5f4f58ba6ff8a679457dacce780bab7
jakep5 <jakepagel1@yahoo.com>
2019-10-22 19:28:09 -0500

    organize code/initialize component code

---
+++ src/BookDescription/BookDescription.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class BookDescription extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default BookDescription
