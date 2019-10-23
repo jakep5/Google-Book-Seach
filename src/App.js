c009df2ce86951c0bdde7a6c31491aab11a32544
jakep5 <jakepagel1@yahoo.com>
2019-10-23 10:32:05 -0500

    reorganize Filter component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import "./App.css";
-import Filters from './Filters/Filters';
-import BookList from './BookList/BookList';
 import SearchBar from './SearchBar/SearchBar';
 
 class App extends React.Component {
@@ -9,26 +7,45 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      searchTerm: '',
       books : [],
       showBookDescription: false,
       printType: 'all',
       bookType: '',
+      url: '',
     }
 
   }
 
-  handleSubmit () {
+  changeSearchTerm (term) {
+    this.setState = ({
+       searchTerm: term
+    })
+  }
 
+  changePrintTypeFilter (type) {
+    this.setState = ({
+      printType: type,
+    })
+  }
+  
+  changeBookTypeFilter (bookType) {
+    this.setState = ({
+      bookType: bookType,
+    })
   }
   
   componentDidMount() {
     if (this.state.bookType = '') {
-      const url = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyAxOUySM6fNOlFXHaxvO7wiaZTkanchXus&q=' + book + '&printType=' + this.state.printType;
+      this.setState({
+        url: 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyAxOUySM6fNOlFXHaxvO7wiaZTkanchXus&q=' + this.state.searchTerm + '&printType=' + this.state.printType,
+      })
     } else {
-      const url = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyAxOUySM6fNOlFXHaxvO7wiaZTkanchXus&q=' + book + '&printType=' + this.state.printType + '&filter=' + bookType;
+      this.setState({
+        url: 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyAxOUySM6fNOlFXHaxvO7wiaZTkanchXus&q=' + this.state.searchTerm + '&printType=' + this.state.printType + '&filter=' + this.state.bookType,
+    })
     }
-
-    fetch (url) 
+    fetch (this.state.url) 
       .then (response => {
         if(!response.ok) {
           throw new Error ('Something went wrong, please try again.')
@@ -56,9 +73,10 @@ class App extends React.Component {
           <div className = "title">
             <h1>Google Book Search</h1>
           </div>
-          <SearchBar />
-          <Filters />
-          <BookList />
+          <SearchBar 
+            handleSearchTerm = {term  => this.changeSearchTerm(term)}
+            handlePrintType = {type => this.changePrintTypeFilter(type)}
+            handleBookType = {bookType => this.changeBookTypeFilter(bookType)}/>
         </div>
       </main>
     );
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react'
 import './SearchBar.css'
+import Filters from '../Filters/Filters';
+import BookList from '../BookList/BookList';
 
 export default class SearchBar extends Component {
 
-    handleSubmit(e) {
-        e.preventDefault();
-
-    }
+ 
     render() {
         return (
-            <div className = "searchBar">
-                <form classname = "searchForm" id = "searchForm">
-                    <label htmlFor="bookTitle">Search:</label>
-                    <input 
-                        type="text"
-                        name="bookTitle"
-                        placeholder = "Book Title"
-                        id="bookTitle"
-                        onChange = {e => this.props.handleSearchTerm(e.target.value)}
-                    >
-                    </input>
-                    <button type="submit" htmlFor = "searchForm">Search</button>
-                </form>
+            <div>
+                <div className = "searchBar">
+                    <form 
+                        className = "searchForm" 
+                        id = "searchForm"
+                        onSubmit = {e => {
+                            e.preventDefault();
+                            this.props.handleSearchTerm(document.getElementById('bookTitle').value)
+                        }}>
+                        <label htmlFor="bookTitle">Search:</label>
+                        <input 
+                            type="text"
+                            name="bookTitle"
+                            placeholder = "Book Title"
+                            ref = 'searchTerm'
+                            id="bookTitle"
+                        >
+                        </input>
+                        <button type="submit" htmlFor="searchForm">Search</button>
+                    </form>
+                </div>
+                <Filters />
             </div>
+            
         )
     }
 }
