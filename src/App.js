93ba8dab38b67bf33ccb2827911e08b27810fac4
jakep5 <jakepagel1@yahoo.com>
2019-10-23 09:37:40 -0500

    add fetch function and state in app.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,56 @@ import Filters from './Filters/Filters';
 import BookList from './BookList/BookList';
 import SearchBar from './SearchBar/SearchBar';
 
-function App() {
-  return (
-    <main className='App'>
-      <div className = "App">
-        <div className = "title">
-          <h1>Google Book Search</h1>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books : [],
+      showBookDescription: false,
+    }
+
+  }
+
+  handleSubmit () {
+
+  }
+  
+  componentDidMount() {
+    const url = 'https://www.googleapis.com/books/v1/volumes?key=AIzaSyAxOUySM6fNOlFXHaxvO7wiaZTkanchXus&q=' + book + '&printType=' + printType + '&filter=' + bookType;
+
+    fetch (url) 
+      .then (response => {
+        if(!response.ok) {
+          throw new Error ('Something went wrong, please try again.')
+        }
+        else {
+          return response
+        }
+      })
+      .then (response => response.json())
+      .then(console.log(response))
+      .catch (err => {
+        this.setState({
+          error: err.message
+        })
+      })
+    }
+  
+  render() {
+    return (
+      <main className='App'>
+        <div className = "App">
+          <div className = "title">
+            <h1>Google Book Search</h1>
+          </div>
+          <SearchBar />
+          <Filters />
+          <BookList />
         </div>
-        <SearchBar />
-        <Filters />
-        <BookList />
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
